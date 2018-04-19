4cb15e8e53b5b878f7f3ee60e919a7fa286d2167
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-04-19 06:07:26 -0400

    Get templates up and running.

--- process-cv.rb
+++ process-cv.rb
@@ -3,38 +3,53 @@
 
 require 'yaml'
 
-# Load in data sources
+# Load in data sources & dump them to a new file.
 data = {}
 Dir.glob('data/*.yml') do |file|
   key = file.sub(/^data\//, "").sub(/\.yml$/, "")
   data[key] = YAML.load_file file
+end
+File.open('metadata.yml', 'w') do |file|
+  file.puts YAML::dump(data)
+  file.puts "---"
+end
+
+# Set the templates
+if data["format"]["mode"] == "markdown"
+  tex_template = "templates/tex.tex"
+  html_template = "templates/html.html"
+else
+  raise "Formatting mode must be 'markdown'."
 end
 
 # Concatenate the sections
 files = data["format"]["cv-sections"].map{ |section| "sections/#{section}.md" }.join " "
 
 # Add templating information
-tex_opts = ""
-pdf_opts = "--pdf-engine=xelatex"
-html_opts = ""
-# tex_opts = "--template=#{data["format"]["template"]} "
-# pdf_opts = "--pdf-engine=xelatex --template=#{data["format"]["template"]} "
-# html_opts = "--template=#{data["format"]["template-html"]}"
+# tex_opts = ""
+# pdf_opts = "--pdf-engine=xelatex"
+# html_opts = ""
+tex_opts = "--template=#{tex_template} "
+pdf_opts = "--pdf-engine=xelatex --template=#{tex_template} "
+html_opts = "--template=#{html_template}"
 
 metadata = "--metadata=title:'#{data["format"]["title"]}' --metadata=author:'#{data["format"]["author"]}' --metadata=date:'#{Time.now.strftime "%F"}'"
 
 tex_cmd = "pandoc -sr markdown+yaml_metadata_block \
   #{tex_opts} \
+  'metadata.yml' \
   #{files} \
   #{metadata} \
   -o docs/out.tex"
 pdf_cmd = "pandoc -sr markdown+yaml_metadata_block \
   #{pdf_opts} \
+  'metadata.yml' \
   #{files} \
   #{metadata} \
   -o docs/#{data["format"]["pdf-options"]["filename"]}.pdf"
 html_cmd = "pandoc -sr markdown+yaml_metadata_block \
   #{html_opts} \
+  'metadata.yml' \
   #{files} \
   #{metadata} \
   -o docs/index.html"
