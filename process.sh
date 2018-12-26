5c65121bec27c5bc953936f67d325a53d2ee51e1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-12-26 10:42:20 -0500

    Format lines

--- process.sh
+++ process.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Based on https://github.com/denten-bin/write-support
 
-# This shell script processes the files in this repository to generate a few temporary files and a final pdf and html file for a CV.
+# This shell script processes the files in this repository to generate a few
+# temporary files and a final pdf and html file for a CV.
 
 # 1. reset tmp directory.
 if [[ -d tmp ]]; then
