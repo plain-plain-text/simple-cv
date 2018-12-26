aad3e9c7a30796807fa372bbfa1b21a3a92f0fef
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-12-26 08:41:57 -0500

    Create metadata file and don't use a goofy variable for tmp.

--- process.sh
+++ process.sh
@@ -2,12 +2,14 @@
 # Based on https://github.com/denten-bin/write-support
 
 # This shell script processes the files in this repository to generate a few temporary files and a final pdf and html file for a CV.
-TEMPDIR="tmp"
 
 # 1. reset tmp directory.
-if [[ -d "$TEMPDIR" ]]; then
+if [[ -d tmp ]]; then
   # The directory exists, so empty its contents
-  rm "$TEMPDIR"/*
+  rm tmp/*
 else
-  mkdir "$TEMPDIR"
+  mkdir tmp
 fi
+
+# 2. collapse data/yml files into one & get rid of ---
+cat data/*.yml | sed '/^---/d' > tmp/metadata.yml
